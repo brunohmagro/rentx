e00b0a79c7c0930e380de53d54fbfb651380f678
Bruno Henrique Magro <brunohmagro@hotmail.com>
2021-06-18 17:37:00 -0300

    chore: configure eslint and prettier in project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+}
