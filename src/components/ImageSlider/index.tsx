f6ff03621cb6a46b08f43d813dc919544712ec1c
Bruno Henrique Magro <brunohmagro@hotmail.com>
2022-02-01 09:00:40 -0300

    feat: add slide animation at car images

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { FlatList } from 'react-native'
 
-import { ImageSliderProps } from './interfaces'
+import { ImageSliderProps, ChangeImageProps } from './interfaces'
 
 import { Container, ImageIndexes, ImageIndex, CarImageWrapper, CarImage } from './styles'
 
 export const ImageSlider: React.FC<ImageSliderProps> = ({ imageUrl }: ImageSliderProps) => {
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = Number(info.viewableItems[0].index)
+    setImageIndex(index)
+  })
+
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {imageUrl.map((_, index) => (
+          <ImageIndex key={String(index)} active={index === imageIndex} />
+        ))}
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage source={{ uri: imageUrl[0] }} resizeMode="contain" />
-      </CarImageWrapper>
+      <FlatList
+        data={imageUrl}
+        keyExtractor={key => key}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item }} resizeMode="contain" />
+          </CarImageWrapper>
+        )}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </Container>
   )
 }
--- src/components/ImageSlider/interfaces.ts
+++ src/components/ImageSlider/interfaces.ts
@@ -1,7 +1,14 @@
+import { ViewToken } from 'react-native'
+
 export interface ImageIndexProps {
   active: boolean
 }
 
 export interface ImageSliderProps {
   imageUrl: string[]
+}
+
+export interface ChangeImageProps {
+  viewableItems: ViewToken[]
+  changed: ViewToken[]
 }
