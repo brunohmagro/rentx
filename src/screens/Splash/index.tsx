3601d34cc2e193e4926b9aba0c6086d05970eb48
Bruno Henrique Magro <brunohmagro@hotmail.com>
2022-02-02 18:26:06 -0300

    feat: improve animation spash screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,53 +1,66 @@
-import React from 'react'
-import { Button, StyleSheet, Dimensions } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/core'
+
+import BrandSvg from '../../assets/brand.svg'
+import LogoSvg from '../../assets/logo.svg'
 
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  interpolate,
   withTiming,
-  Easing,
+  Extrapolate,
+  runOnJS,
 } from 'react-native-reanimated'
-
-const WITH = Dimensions.get('window').width
 
 import { Container } from './styles'
 
 export const Splash: React.FC = () => {
-  const animation = useSharedValue(0)
+  const navigation = useNavigation()
+  const splashAnimation = useSharedValue(0)
 
-  const animatedStyles = useAnimatedStyle(() => {
+  const brandStyle = useAnimatedStyle(() => {
     return {
+      opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
       transform: [
         {
-          translateX: withTiming(animation.value, {
-            duration: 500,
-            easing: Easing.bounce,
-          }),
+          translateX: interpolate(splashAnimation.value, [0, 50], [0, -50], Extrapolate.CLAMP),
         },
       ],
     }
   })
 
-  const handleAnimationPosition = () => {
-    if (animation.value >= WITH - 100) {
-      animation.value = 0
-    } else {
-      animation.value += 100
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value, [0, 50], [-50, 0], Extrapolate.CLAMP),
+        },
+      ],
     }
+  })
+
+  const startApp = () => {
+    navigation.navigate('Home')
   }
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, { duration: 2000 }, () => {
+      'worklet'
+      runOnJS(startApp)()
+    })
+  }, [])
 
   return (
     <Container>
-      <Animated.View style={[styles.box, animatedStyles]} />
-      <Button title="Mover" onPress={handleAnimationPosition} />
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+        <BrandSvg width={80} height={50} />
+      </Animated.View>
+
+      <Animated.View style={[logoStyle, { position: 'absolute' }]}>
+        <LogoSvg width={180} height={20} />
+      </Animated.View>
     </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-  },
-})
